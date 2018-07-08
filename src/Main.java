ed8adb93995c139a26c555cfbe0820a60c2ffa45
andresmontoyab <and.montoya18@gmail.com>
2018-07-08 16:05:08 -0500

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /// First Step
+
+        int numer1 = 0; int numer2 = 0;
+        Scanner valisteVerga = new Scanner(System.in);
+
+        System.out.println("Ingrese primer numero ");
+        numer1 = valisteVerga.nextInt();
+
+        System.out.println("Ingrese segundo numero");
+        numer2 = valisteVerga.nextInt();
+
+        int respuesta =numer1+numer2;
+        System.out.println("repuesta: "+ respuesta+" final");
+
+
+
+
+
+
+    }
+}
